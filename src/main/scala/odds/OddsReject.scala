7219ba9a08b8be63d5a3fdc2d3bb4cebc936bc7f
Sandro Stucki <sandro.stucki@gmail.com>
2013-04-13 17:29:33 +0200

    WIP: Adds simple rejection based sampling (needs test cases).

---
+++ src/main/scala/odds/OddsReject.scala
@@ -0,0 +1,41 @@
+package odds
+
+import scala.collection.mutable
+
+/** Simple, rejection sampler for the ODDS language. */
+trait OddsReject extends OddsIntf with DistIterables {
+
+  val prng = new java.util.Random
+
+  type Rand[+A] = RandVar[A]
+
+  final case class RandVar[+A](v: Option[A]) extends RandIntf[A] {
+
+    def flatMap[B](f: A => Rand[B]): Rand[B] = RandVar(v.flatMap(f(_).v))
+
+    def orElse[B >: A](that: Rand[B]): Rand[B] = RandVar(this.v orElse that.v)
+  }
+
+  def choice[A](xs: (A, Prob)*): Rand[A] = if (xs.isEmpty) RandVar(None) else {
+    val (vals, weights) = xs.unzip
+    val idx = DistTree(weights, prng).nextRandom._1
+    RandVar(Some(vals(idx)))
+  }
+
+  /**
+   * Approximate the distribution defined by a probabilistic
+   * computation.
+   *
+   * @param samples the number of samples used to approximate the
+   *        distribution.
+   * @return an approximation of the distribution over the values
+   *         of this random variable.
+   */
+  def sample[A](samples: Int)(x: => Rand[A]): Dist[A] = {
+    val distMap = new mutable.HashMap[A, Prob]()
+    for (_ <- 1 to samples; v <- x.v) {
+      distMap(v) = distMap(v) + 1
+    }
+    distMap
+  }
+}
