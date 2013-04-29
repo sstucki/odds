667ce984be904acbe550dfe515e0d05fa90812c2
Sandro Stucki <sandro.stucki@gmail.com>
2013-04-29 14:37:42 +0200

    Adds 'normalize' parameter to pretty printer.

--- src/main/scala/odds/OddsLang.scala
+++ src/main/scala/odds/OddsLang.scala
@@ -53,17 +53,17 @@ trait OddsLang extends EmbeddedControls with OddsIntf with DistIntf {
 trait OddsPrettyPrint {
   this: DistIntf =>
 
-  def pp[A](dist: Dist[A]) = {
-    val d = normalize(dist)
+  def pp[A](dist: Dist[A], normalize: Boolean) = {
+    val d = if (normalize) this.normalize(dist) else dist
     val dSorted = d.toList.sortBy{ case (x, p) => (-p, x.toString) }
     (dSorted map {
       case (x, p) => x + " : " + p
     }).mkString("\n")
   }
 
-  def show[A](dist: Dist[A], desc: String = "") = {
+  def show[A](dist: Dist[A], desc: String = "", normalize: Boolean = false) = {
     println(desc)
-    println(pp(dist))
+    println(pp(dist, normalize))
     println("")
   }
 }
