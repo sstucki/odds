f258d1eb61ca28783375c321e7fdf9764f192ff6
Sandro Stucki <sandro.stucki@gmail.com>
2013-06-27 21:33:46 +0200

    Trivial changes in CommittedChoice interface.

--- src/main/scala/odds/CommittedChoice.scala
+++ src/main/scala/odds/CommittedChoice.scala
@@ -9,21 +9,22 @@ trait CommittedChoice[+A] {
   val id = freshId
 
   /** Find a particular choice this random variable might be committed to. */
-  def choice(env: Environment): Option[A] =
+  @inline final def choice(env: Environment): Option[A] =
     env.get(id).map(_.asInstanceOf[A])
 
   /** Commit to a given choice of value. */
-  def commit[B >: A](env: Environment, v: B): Environment = {
+  @inline final def commit[B >: A](env: Environment, v: B): Environment = {
     env.updated(id, v)
   }
 
   /** Renounce the commitment to a specific value. */
-  def relax(env: Environment): Environment = {
+  @inline final def relax(env: Environment): Environment = {
     env - id
   }
 
   /** Execute a closure with a given commitment. */
-  def withChoice[B >: A, C](env: Environment, v: B)(f: Environment => C): C = {
+  @inline final def withChoice[B >: A, C](env: Environment, v: B)(
+    f: Environment => C): C = {
     val env1 = commit(env, v)
     val r = f(env1)
     r
@@ -36,12 +37,15 @@ object CommittedChoice {
   type Environment = scala.collection.immutable.Map[Long, Any];
 
   /** Counter to generate new IDs for [[CommittedChoice]] instances. */
-  var idCounter: Long = 0
+  private[this] var _idCounter: Long = 0
+
+  /** Counter to generate new IDs for [[CommittedChoice]] instances. */
+  def idCounter = _idCounter
 
   /** Generate a fresh ID for a [[CommittedChoice]] instances. */
   def freshId: Long = {
-    val id = idCounter
-    idCounter += 1
+    val id = _idCounter
+    _idCounter += 1
     id
   }
 }
