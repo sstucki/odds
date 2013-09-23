139490d5f0812942aac3488fb7809051a40d002d
Sandro Stucki <sandro.stucki@gmail.com>
2013-09-23 15:17:42 +0200

    Temporary fix to make virtualiyation of `Any` API work with Scala Virtualized.

--- macros/src/main/scala/odds/macros/VirtualAny.scala
+++ macros/src/main/scala/odds/macros/VirtualAny.scala
@@ -31,24 +31,62 @@ trait VirtualAny extends EmbeddedControls { this: OddsBase =>
   import VirtualAny.MacrosInOddsBase._
 
   // Poor man's infix methods for `Any` methods
-  def __equal(x1: Any, x2: Rand[Any]): Any = macro any_==
-  def __equal(x1: Rand[Any], x2: Any): Any = macro any_==
-  def __equal(x1: Rand[Any], x2: Rand[Any]): Any = macro any_==
-  def infix_==(x1: Any, x2: Rand[Any]): Any = macro any_==
-  def infix_==(x1: Rand[Any], x2: Any): Any = macro any_==
-  def infix_==(x1: Rand[Any], x2: Rand[Any]): Any = macro any_==
-  def infix_!=(x1: Any, x2: Rand[Any]): Any = macro any_!=
-  def infix_!=(x1: Rand[Any], x2: Any): Any = macro any_!=
-  def infix_!=(x1: Rand[Any], x2: Rand[Any]): Any = macro any_!=
-  def infix_##(x: Rand[Any]): Any = macro any_##
-  def infix_equals(x1: Any, x2: Rand[Any]): Any = macro any_equals
-  def infix_equals(x1: Rand[Any], x2: Any): Any = macro any_equals
-  def infix_equals(x1: Rand[Any], x2: Rand[Any]): Any = macro any_equals
-  def infix_hashCode(x: Rand[Any]): Any = macro any_hashCode
-  def infix_asInstanceOf[T](x: Rand[Any]): Any = macro any_asInstanceOf[T]
-  def infix_isInstanceOf[T](x: Rand[Any]): Any = macro any_isInstanceOf[T]
-  def infix_toString(x: Rand[Any]): Any = macro any_toString
-  def infix_getClass(x: Rand[Any]): Any = macro any_getClass
+
+  // FIXME: sstucki: Unfortunately, the fully generic, macro-based
+  // implementations of the infix functions don't seem to work with
+  // current Scala Virtualized (as of 2013-09-23).  It's not quite
+  // clear to me whether the bug is in Scala Virtualized or macros or
+  // a combination of the two.  Possible solutions:
+  //
+  //  * Isolate and fix the bug in Scala Virtualized and/or macros.
+  //    Might be non-trivial as this requires an investigation into
+  //    how macros and Scala Virtualized are supposed to interact.
+  //
+  //  * Don't use macro implementations.  This will require advanced
+  //    type-level programming if we don't want to abandon deep
+  //    lifting (i.e. lifting across nested monadic types).
+  //
+  //  * Don't use Scala Virtualized.  This requires some other form of
+  //    language virtualization, likely to be macro-based
+  //    e.g. an @virtualize or @odds macro annotation that perform the
+  //    virtualization transformation.
+
+  // def __equal(x1: Any, x2: Any): Any = macro any_==
+  // def infix_==(x1: Any, x2: Any): Any = macro any_==
+  // def infix_!=(x1: Any, x2: Any): Any = macro any_!=
+  // def infix_##(x: Any): Any = macro any_##
+  // def infix_equals(x1: Any, x2: Any): Any = macro any_equals
+  // def infix_hashCode(x: Any): Any = macro any_hashCode
+  // def infix_asInstanceOf[T](x: Any): Any = macro any_asInstanceOf[T]
+  // def infix_isInstanceOf[T](x: Any): Any = macro any_isInstanceOf[T]
+  // def infix_toString(x: Any): Any = macro any_toString
+  // def infix_getClass(x: Any): Any = macro any_getClass
+
+  // FIXME (cont.): These are method-based implementation of the infix
+  // methods corresponding to the `Any` API.  Proper lifting relies on
+  // overloading resolution on parameter types: any combination of
+  // plain and monadic argument types needs to be implemented to
+  // ensure the appropriate lifting is used.  Unfortunately, this
+  // technique does not handle deep lifting correctly.
+  import Rand._
+  def __equal(x1: Any, rx2: Rand[Any]): Rand[Boolean] = fmap(rx2)(x1 ==  _)
+  def __equal(rx1: Rand[Any], x2: Any): Rand[Boolean] = fmap(rx1)(_ == x2)
+  def __equal(rx1: Rand[Any], rx2: Rand[Any]): Rand[Boolean] = bind(rx2)(rx1 == _)
+  def infix_==(x1: Any, rx2: Rand[Any]): Rand[Boolean] = fmap(rx2)(x1 == _)
+  def infix_==(rx1: Rand[Any], x2: Any): Rand[Boolean] = fmap(rx1)(_ == x2)
+  def infix_==(rx1: Rand[Any], rx2: Rand[Any]): Rand[Boolean] = bind(rx2)(rx1 == _)
+  def infix_!=(x1: Any, rx2: Rand[Any]): Rand[Boolean] = fmap(rx2)(x1 != _)
+  def infix_!=(rx1: Rand[Any], x2: Any): Rand[Boolean] = fmap(rx1)(_ != x2)
+  def infix_!=(rx1: Rand[Any], rx2: Rand[Any]): Rand[Boolean] = bind(rx2)(rx1 != _)
+  def infix_##(rx: Rand[Any]): Rand[Int] = fmap(rx)(_.##)
+  def infix_equals(x1: Any, rx2: Rand[Any]): Rand[Boolean] = fmap(rx2)(x1 equals _)
+  def infix_equals(rx1: Rand[Any], x2: Any): Rand[Boolean] = fmap(rx1)(_ equals x2)
+  def infix_equals(rx1: Rand[Any], rx2: Rand[Any]): Rand[Boolean] = bind(rx2)(rx1 equals _)
+  def infix_hashCode(rx: Rand[Any]): Rand[Int] = fmap(rx)(_.hashCode)
+  def infix_asInstanceOf[T](rx: Rand[Any]): Rand[T] = fmap(rx)(_.asInstanceOf[T])
+  def infix_isInstanceOf[T](rx: Rand[Any]): Rand[Boolean] = fmap(rx)(_.isInstanceOf[T])
+  def infix_toString(rx: Rand[Any]): Rand[String] = fmap(rx)(_.toString)
+  def infix_getClass(rx: Rand[Any]): Rand[java.lang.Class[_]] = fmap(rx)(_.getClass)
 }
 
 /**
